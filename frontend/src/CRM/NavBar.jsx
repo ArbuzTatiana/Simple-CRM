4e7dfce883a399630005e59b6275ea0ec3b3b67b
ArbuzTatiana <work.arbuztatiana@gmail.com>
2021-06-21 21:27:23 +0300

    add routing to tthe project

---
+++ frontend/src/CRM/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from "./Nav";
+
+
+const NavBar = () => {
+    return (
+        <div className="navBar">
+            <h1>CRM</h1>
+            <Nav />
+        </div>
+
+    )
+};
+
+export default NavBar;
---
+++ frontend/src/CRM/Routing.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route} from "react-router-dom";
+import Home from './Pages/Home';
+import Companies from './Pages/Companies';
+import Employees from './Pages/Employees';
+
+const Routing = () => {
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+            <Route path="/companies">
+                <Companies/>
+            </Route>
+            <Route path="/employees">
+                <Employees/>
+            </Route>
+        </Switch>
+    )
+};
+
+export default Routing;
